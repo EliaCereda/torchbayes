3e75f6aab98acdc1ceafa3705726e21fea0839c9
Elia Cereda <eliacereda@gmail.com>
2020-10-09 18:39:35 +0200

    Updated MNIST.Model to directly inherit from nn.Sequential instead of nesting one

--- examples/MNIST/model.py
+++ examples/MNIST/model.py
@@ -7,13 +7,11 @@ from torchbayes import bnn
 from torchbayes.distributions import ScaleMixtureNormal
 
 
-class Model(bnn.BayesModel):
+class Model(bnn.BayesModel, nn.Sequential):
     """Architecture used by [Blundell'15] for the experiments on MNIST."""
 
     def __init__(self, in_shape, out_features):
-        super().__init__()
-
-        self.net = nn.Sequential(
+        super().__init__(
             nn.Flatten(),
 
             bnn.BayesLinear(math.prod(in_shape), 1200),
@@ -36,6 +34,3 @@ class Model(bnn.BayesModel):
         # self.apply(bnn.init_posteriors(
         #     Normal, loc=Normal(0.0, 0.01), scale=Normal(math.exp(-2), 0.01)
         # ))
-
-    def forward(self, input):
-        return self.net(input)
