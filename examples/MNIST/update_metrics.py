5f0796e081c16b406d54acec28d4b67251dc0c05
Elia Cereda <eliacereda@gmail.com>
2020-11-11 21:42:08 +0100

    Update update_metrics.py to compute a combined metric that evaluates both accuracy and entropy_auc

--- examples/MNIST/update_metrics.py
+++ examples/MNIST/update_metrics.py
@@ -11,7 +11,7 @@ import wandb.apis.public as wandb_api
 
 
 # TODO: sync with training script
-CURRENT_VERSION = 1
+CURRENT_VERSION = 2
 
 
 def histogram_to_preds(histogram):
@@ -47,46 +47,87 @@ def add_entropy_roc(run, plots_dir):
     fpr, tpr, _ = metrics.roc_curve(targets, preds)
     roc_auc = metrics.auc(fpr, tpr)
 
-    fig, ax = plt.subplots()
-    metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc).plot(ax, name=run.name)
-
     tqdm.write(f"   + ROC AUC: {roc_auc}")
-    # FIXME: unfortunately adding plots or images in retrospect is not supported
-    # run.summary['valid/entropy_roc'] = wandb.Image(fig)
     run.summary['valid/entropy_auc'] = roc_auc
 
-    # Save the ROC plot locally
-    roc_dir = os.path.join(plots_dir, 'entropy_roc')
-    os.makedirs(roc_dir, exist_ok=True)
-    fig.savefig(os.path.join(roc_dir, f'{run.id}_{run.name}.pdf'))
-    plt.close(fig)
+    if plots_dir:
+        fig, ax = plt.subplots()
+        metrics.RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc).plot(ax, name=run.name)
+
+        # FIXME: unfortunately adding plots or images in retrospect is not supported
+        # run.summary['valid/entropy_roc'] = wandb.Image(fig)
+
+        # Save the ROC plot locally
+        roc_dir = os.path.join(plots_dir, 'entropy_roc')
+        os.makedirs(roc_dir, exist_ok=True)
+        fig.savefig(os.path.join(roc_dir, f'{run.id}_{run.name}.pdf'))
+        plt.close(fig)
+
+
+def add_combined_score(run):
+    accuracy = run.summary['valid/accuracy']
+    auc = run.summary['valid/entropy_auc']
+
+    score = np.linalg.norm([
+        1 - accuracy,
+        1 - auc
+    ])
+
+    tqdm.write(f"   + Combined score: {score}")
+    run.summary['valid/combined_score'] = score
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('sweep', help="Path of the sweep to be processed, in the form entity_id/project_id/sweep_id.")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--project', help="Path of the project, in the form entity_id/project_id.")
+    group.add_argument('--sweep', help="Path of the sweep to be processed, in the form entity_id/project_id/sweep_id.")
+
+    parser.add_argument('--tag', help="Only select runs with a certain tag.")
+
     parser.add_argument('--dry-run', action='store_true',
                         help="Describe the changes without actually performing them.")
     args = parser.parse_args()
 
     api = wandb.Api()
-    sweep: wandb_api.Sweep = api.sweep(args.sweep)
 
-    print(f"Processing sweep {sweep.url}")
+    if args.project:
+        filters = {}
 
-    plots_dir = os.path.join('update_metrics', sweep.id)
+        if args.tag:
+            filters['tags'] = args.tag
+
+        runs = api.runs(args.project, filters=filters)
+        plots_dir = os.path.join('update_metrics', args.project)
+    elif args.sweep:
+        sweep: wandb_api.Sweep = api.sweep(args.sweep)
+
+        print(f"Processing sweep {sweep.url}")
+
+        runs = sweep.runs
+        plots_dir = os.path.join('update_metrics', sweep.id)
+    else:
+        raise ValueError("One of --project or --sweep must be provided.")
 
     run: wandb_api.Run
-    for run in tqdm(sweep.runs):
+    for run in tqdm(runs):
+        version = run.config.get('metrics_version', 0)
+
+        if version == CURRENT_VERSION:
+            continue
+
         tqdm.write(f"Run {run.name}:")
         tqdm.write(f" - URL {run.url}")
-
-        version = run.config.get('metrics_version', 0)
         tqdm.write(f" - current metrics version v{version}")
 
         if version < 1:
             tqdm.write(f" - adding entropy discrimination ROC curve")
             add_entropy_roc(run, plots_dir)
+
+        if version < 2:
+            tqdm.write(f" - adding accuracy / AUC combined score")
+            add_combined_score(run)
 
         run.config['metrics_version'] = CURRENT_VERSION
 
