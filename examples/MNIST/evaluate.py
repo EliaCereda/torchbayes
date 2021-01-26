e3a23cf3ac6ef4e6715ea3e8a891120e94420de5
Elia Cereda <eliacereda@gmail.com>
2021-01-26 12:17:45 +0100

    Support disabling the cutout in evaluate.py

--- examples/MNIST/evaluate.py
+++ examples/MNIST/evaluate.py
@@ -62,63 +62,83 @@ def main():
     ood_entropy_auc(entropy_id, entropy_ood)
 
 
-def ood_entropy_auc(entropy_id, entropy_ood):
+def ood_entropy_auc(entropy_id, entropy_ood, broken=False):
     target_id = np.zeros_like(entropy_id)
     target_ood = np.ones_like(entropy_ood)
 
     entropy = np.concatenate([entropy_id, entropy_ood])
     targets = np.concatenate([target_id, target_ood])
 
+    # Histogram plot with a broken axis
+    # Source: https://matplotlib.org/3.3.3/gallery/subplots_axes_and_figures/broken_axis.html
     fig: plt.Figure = plt.figure(figsize=(9.0, 4.5))
     gs = plt.GridSpec(1, 2, wspace=0.3, figure=fig)
-    gs1 = gs[0].subgridspec(2, 1, hspace=-0.15)
 
-    ax11: plt.Axes = fig.add_subplot(gs1[0])
-    ax12: plt.Axes = fig.add_subplot(gs1[1], sharex=ax11)
-    ax2: plt.Axes = fig.add_subplot(gs[1])
+    if broken:
+        gs1 = gs[0].subgridspec(2, 1, hspace=-0.15)
+        ax11: plt.Axes = fig.add_subplot(gs1[0])
+        ax12: plt.Axes = fig.add_subplot(gs1[1], sharex=ax11)
+        axs1 = [ax11, ax12]
+    else:
+        ax1: plt.Axes = fig.add_subplot(gs[0])
+        axs1 = [ax1]
 
-    for ax in [ax11, ax12]:
+    for ax in axs1:
         ax.hist(entropy_id, 96, alpha=0.8, label="in domain (MNIST)")
         ax.hist(entropy_ood, 96, alpha=0.8, label="out of domain (Fashion-MNIST)")
 
         ax.grid(True)
         ax.set_axisbelow(True)
 
-    ax11.legend()
-    # Show y label centered at the bottom of the upper subplot, so it appears at
-    # the center of the overall figure.
-    # Using the upper subplot because it should generally have longer tick
-    # labels, so the y label is positioned correctly.
-    ax11.set_ylabel("Sample count", y=0, horizontalalignment='center')
-    ax12.set_xlabel("Entropy")
+    if broken:
+        ax11.legend()
 
-    # zoom-in / limit the view to different portions of the data
-    max_inlier = 800
-    y0, y1 = ax11.get_ylim()
-    ax11.set_ylim(bottom=y1 - max_inlier)  # outliers only
-    ax12.set_ylim(0, max_inlier)  # most of the data
+        # Show y label centered at the bottom of the upper subplot, so it appears at
+        # the center of the overall figure.
+        # Using the upper subplot because it should generally have longer tick
+        # labels, so the y label is positioned correctly.
+        ax11.set_ylabel("Sample count", y=0, horizontalalignment='center')
+        ax12.set_xlabel("Entropy")
 
-    for ax in [ax11, ax12]:
+        # zoom-in / limit the view to different portions of the data
+        max_inlier = 800
+        y0, y1 = ax11.get_ylim()
+        ax11.set_ylim(bottom=y1 - max_inlier)  # outliers only
+        ax12.set_ylim(0, max_inlier)  # most of the data
+
+        # Modify the aspect ratio so that the plot is squared like the ROC curve
+        for ax in [ax11, ax12]:
+            x0, x1 = ax.get_xlim()
+            y0, y1 = ax.get_ylim()
+            ax.set_aspect(0.5 * (x1 - x0) / (y1 - y0))
+
+        # hide the spines between ax and ax2
+        ax11.spines['bottom'].set_visible(False)
+        ax12.spines['top'].set_visible(False)
+        ax11.xaxis.set_ticks_position('none')
+        ax11.tick_params(labelbottom=False)  # don't put tick labels at the top
+        ax12.xaxis.tick_bottom()
+
+        # Cut-out slanted lines
+        d = .5  # proportion of vertical to horizontal extent of the slanted line
+        axis_width = plt.rcParams['axes.linewidth']
+        kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linewidth=axis_width,
+                      linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+        ax11.plot([0, 1], [0, 0], transform=ax11.transAxes, **kwargs)
+        ax12.plot([0, 1], [1, 1], transform=ax12.transAxes, **kwargs)
+    else:
+        # Modify the aspect ratio so that the plot is squared like the ROC curve
         x0, x1 = ax.get_xlim()
         y0, y1 = ax.get_ylim()
-        ax.set_aspect(0.5 * (x1 - x0) / (y1 - y0))
+        ax1.set_aspect((x1 - x0) / (y1 - y0))
 
-    # hide the spines between ax and ax2
-    ax11.spines['bottom'].set_visible(False)
-    ax12.spines['top'].set_visible(False)
-    ax11.xaxis.set_ticks_position('none')
-    ax11.tick_params(labelbottom=False)  # don't put tick labels at the top
-    ax12.xaxis.tick_bottom()
-
-    # Cut-out slanted lines
-    d = .5  # proportion of vertical to horizontal extent of the slanted line
-    axis_width = plt.rcParams['axes.linewidth']
-    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=12, linewidth=axis_width,
-                  linestyle="none", color='k', mec='k', mew=1, clip_on=False)
-    ax11.plot([0, 1], [0, 0], transform=ax11.transAxes, **kwargs)
-    ax12.plot([0, 1], [1, 1], transform=ax12.transAxes, **kwargs)
+        ax1.legend()
+        ax1.set_ylabel("Sample count")
+        ax1.set_xlabel("Entropy")
 
     ## ROC curve
+    ax2: plt.Axes = fig.add_subplot(gs[1])
+
     fpr, tpr, _ = m.roc_curve(targets, entropy)
     roc_auc = m.auc(fpr, tpr)
 
